91105473804cc8a8e0c9d70c5ebb98b125b03f3e
Patrick <Patrick@Patrick-Fixe>
2022-09-20 09:14:34 +0200

    inscription

--- src/main/java/fr/eni/enchere/servlets/ServletConnexion.java
+++ src/main/java/fr/eni/enchere/servlets/ServletConnexion.java
@@ -32,16 +32,26 @@ public class ServletConnexion extends HttpServlet {
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String  login = request.getParameter("login");
 		String mdp = request.getParameter("mdp");
+		String action = request.getParameter("action");
 		Utilisateur user = null;
 		
-		try {
-			user = UtilisateurManager.getInstance().seConnecter(login, mdp);
-			request.getSession().setAttribute("user", user);
-			response.sendRedirect("http://www.google.com");
-		} catch (BusinessException e) {
-			request.setAttribute("listeCodesErreur", e.getListeCodesErreur());
-			request.getRequestDispatcher("/WEB-INF/jsp/connexion.jsp").forward(request, response);
+		switch (action) {
+		case "connexion":
+			try {
+				user = UtilisateurManager.getInstance().seConnecter(login, mdp);
+				request.getSession().setAttribute("user", user);
+				response.sendRedirect("http://www.google.com");
+			} catch (BusinessException e) {
+				request.setAttribute("listeCodesErreur", e.getListeCodesErreur());
+				request.getRequestDispatcher("/WEB-INF/jsp/connexion.jsp").forward(request, response);
+			}
+			break;
+
+		default:
+			break;
 		}
+		
+
 	}
 
 }
