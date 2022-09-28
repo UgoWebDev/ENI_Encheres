db0f521946e84e4cb69a28d9112717a1ad59e07d
Patrick <Patrick@Patrick-Fixe>
2022-09-28 12:25:37 +0200

    Nom raccord avec le nommage

--- src/main/java/fr/eni/enchere/servlets/ServletGestionDetailsVente.java
+++ src/main/java/fr/eni/enchere/servlets/ServletGestionDetailsVente.java
@@ -10,7 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 /**
  * Servlet implementation class GestionDetailsVente
  */
-@WebServlet("/detailsvente")
+@WebServlet("/detail")
 public class ServletGestionDetailsVente extends HttpServlet {
 	private static final long serialVersionUID = 1L;
 
