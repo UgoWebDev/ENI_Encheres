7f13527678d141829df4f3b94e1899af557a3fc6
Patrick <Patrick@Patrick-Fixe>
2022-09-29 11:47:40 +0200

    Merge branch 'main' of https://github.com/UgoWebDev/Eni_Encheres.git into main

--- src/main/java/fr/eni/enchere/Utilitaires.java
+++ src/main/java/fr/eni/enchere/Utilitaires.java
@@ -16,10 +16,19 @@ public class Utilitaires {
 	}
 
 	public static String toHtml(String chaine) {
-		String[][] html = new String[][] {{" à","&agrave;"},{"À","&Agrave;"},{"á","&aacute;"},{"Á","&Aacute;"},{"â","&acirc; "},{"Â","&Acirc; "},{"ã","&atilde;"},{"Ã","&Atilde;"},{"ä","&auml; "},{"Ä","&Auml; "},{"å","&aring; "},{"Å","&Aring; "},{"æ","&aelig; "},{"Æ","&AElig; "},{"è","&egrave;"},{"È","&Egrave;"},{"é","&eacute;"},{"É","&Eacute;"},{"ê","&ecirc; "},{"Ê","&Ecirc; "},{"ë","&euml; "},{"Ë","&Euml; "},{"ì","&igrave;"},{"Ì","&Igrave;"},{"í","&iacute;"},{"Í","&Iacute;"},{"î","&icirc; "},{"Î","&Icirc; "},{"ï","&iuml; "},{"Ï","&Iuml; "},{"ò","&ograve;"},{"Ò","&Ograve;"},{"ó","&oacute;"},{"Ó","&Oacute;"},{"ô","&ocirc; "},{"Ô","&Ocirc; "},{"õ","&otilde;"},{"Õ","&Otilde;"},{"ö","&ouml; "},{"Ö","&Ouml; "},{"ø","&oslash;"},{"Ø","&Oslash;"},{"ù","&ugrave;"},{"Ù","&Ugrave;"},{"ú","&uacute;"},{"Ú","&Uacute;"},{"û","&ucirc; "},{"Û","&Ucirc; "},{"ü","&uuml; "},{"Ü","&Uuml; "},{"ñ","&ntilde;"},{"Ñ","&Ntilde;"},{"ç","&ccedil;"},{"Ç","&Ccedil;"},{"ý","&yacute;"},{"Ý","&Yacute;"},{"ß","&szlig; "},{"»","&laquo; "},{"»","&raquo; "},{"&","&amp;"},{"<","&lt;"},{"<","&gt;"},{"\"","&quot;"},{"§","&para; "},{"©","&copy; "},{"  ","&nbsp; "}};
+		String[][] html = new String[][] {{" à","&agrave;"},{"À","&Agrave;"},{"á","&aacute;"},{"Á","&Aacute;"},{"â","&acirc; "},{"Â","&Acirc; "},{"ã","&atilde;"},
+			{"Ã","&Atilde;"},{"ä","&auml; "},{"Ä","&Auml; "},{"å","&aring; "},{"Å","&Aring; "},{"æ","&aelig; "},{"Æ","&AElig; "},{"è","&egrave;"},{"È","&Egrave;"},
+			{"é","&eacute;"},{"É","&Eacute;"},{"ê","&ecirc; "},{"Ê","&Ecirc; "},{"ë","&euml; "},{"Ë","&Euml; "},{"ì","&igrave;"},{"Ì","&Igrave;"},{"í","&iacute;"},
+			{"Í","&Iacute;"},{"î","&icirc; "},{"Î","&Icirc; "},{"ï","&iuml; "},{"Ï","&Iuml; "},{"ò","&ograve;"},{"Ò","&Ograve;"},{"ó","&oacute;"},{"Ó","&Oacute;"},
+			{"ô","&ocirc; "},{"Ô","&Ocirc; "},{"õ","&otilde;"},{"Õ","&Otilde;"},{"ö","&ouml; "},{"Ö","&Ouml; "},{"ø","&oslash;"},{"Ø","&Oslash;"},{"ù","&ugrave;"},
+			{"Ù","&Ugrave;"},{"ú","&uacute;"},{"Ú","&Uacute;"},{"û","&ucirc; "},{"Û","&Ucirc; "},{"ü","&uuml; "},{"Ü","&Uuml; "},{"ñ","&ntilde;"},{"Ñ","&Ntilde;"},
+			{"ç","&ccedil;"},{"Ç","&Ccedil;"},{"ý","&yacute;"},{"Ý","&Yacute;"},{"ß","&szlig; "},{"»","&laquo; "},{"»","&raquo; "},{"&","&amp;"},{"<","&lt;"},{"<","&gt;"},
+			{"\"","&quot;"},{"§","&para; "},{"©","&copy; "},{"  ","&nbsp; "}};
 
 
 		for (int i = 0; i < html.length; i++) {
+			String a = html[i][0];
+			String b = html[i][1];
 			chaine.replaceAll(html[i][0], html[i][1]);
 		}
 		return chaine;
