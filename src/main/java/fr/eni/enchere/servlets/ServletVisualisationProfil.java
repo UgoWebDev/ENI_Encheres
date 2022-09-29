91b2dabe933d5f1c5402a3145b1323e51514931c
Patrick <Patrick@Patrick-Fixe>
2022-09-29 12:04:29 +0200

    suppression import inutile

--- src/main/java/fr/eni/enchere/servlets/ServletVisualisationProfil.java
+++ src/main/java/fr/eni/enchere/servlets/ServletVisualisationProfil.java
@@ -7,7 +7,6 @@ import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
-import javax.websocket.Session;
 
 import fr.eni.enchere.bll.UtilisateurManager;
 import fr.eni.enchere.bo.Utilisateur;
